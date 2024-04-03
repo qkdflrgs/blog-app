6c5311941371fb503940cd7c3765a805f4453101
qkdflrgs <qkdflrgs@gmail.com>
2024-04-03 14:43:35 +0900

    feat(routing): set routing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Post Link</Link>
+        </li>
+        <li>
+          <Link to="/posts/:id">Post Detail</Link>
+        </li>
+        <li>
+          <Link to="/posts/new">Post New</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+        <Route path="/posts" element={<h1>Post List</h1>} />
+        <Route path="/posts/:id" element={<h1>Post Detail</h1>} />
+        <Route path="/posts/new" element={<h1>Post New</h1>} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
   );
 }
 
