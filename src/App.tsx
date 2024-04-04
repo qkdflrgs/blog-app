cc38d25e2e270b06dc571107f2a7869cc95fa0ba
qkdflrgs <qkdflrgs@gmail.com>
2024-04-04 15:34:58 +0900

    feat(firebase-auth): add Firebase-auth's onAuthStateChanged to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import Router from "components/Router";
 import { app } from "firebaseApp";
-import { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "components/Loader";
 
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <>
-      <Router isAuthenticated={isAuthenticated} />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
