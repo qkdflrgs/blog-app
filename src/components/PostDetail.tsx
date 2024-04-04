4fc8ad58a16ba1a6ff1ac83d4a6de9f2c14f5059
qkdflrgs <qkdflrgs@gmail.com>
2024-04-05 03:20:38 +0900

    feat(firestore): add deleteDoc to PostDetail component

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { PostProps } from "./PostList";
-import { doc, getDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "firebaseApp";
 import Loader from "./Loader";
+import { toast } from "react-toastify";
 
 export default function PostDetail() {
   const [post, setPost] = useState<PostProps | null>(null);
   const params = useParams();
+  const navigate = useNavigate();
 
   const getPost = async (id: string) => {
     if (id) {
@@ -18,8 +20,13 @@ export default function PostDetail() {
     }
   };
 
-  const handleDelete = () => {
-    console.log("delete");
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast.success("게시글을 삭제했습니다");
+      navigate("/");
+    }
   };
 
   useEffect(() => {
@@ -46,7 +53,7 @@ export default function PostDetail() {
                 삭제
               </div>
               <div className="post__edit">
-                <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                <Link to={`/posts/edit/${post.id}`}>수정</Link>
               </div>
             </div>
             <div className="post__text post__text--prev-wrap">
