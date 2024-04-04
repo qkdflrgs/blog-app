1ab2cb7d1f68ea7d4e32e9d577405aeed0f76a94
qkdflrgs <qkdflrgs@gmail.com>
2024-04-04 11:54:38 +0900

    refactor(router): add Authenticated state

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -6,18 +6,30 @@ import PostNew from "../pages/posts/new";
 import ProfilePage from "../pages/profile";
 import SignInPage from "../pages/signin";
 import SignUpPage from "../pages/signup";
+import { useState } from "react";
 
 export default function Router() {
+  // To Do: firebase Auth 인증이 완료되면 true로 변경되는 로직 추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/posts" element={<PostList />} />
-      <Route path="/posts/:id" element={<PostDetail />} />
-      <Route path="/posts/new" element={<PostNew />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
+      {isAuthenticated ? (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/posts" element={<PostList />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+          <Route path="/posts/new" element={<PostNew />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<Navigate replace to="/signin" />} />
+        </>
+      )}
     </Routes>
   );
 }
