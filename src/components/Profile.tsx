92478c39cbb389b797f1be365184776432a2eee6
qkdflrgs <qkdflrgs@gmail.com>
2024-04-04 22:38:14 +0900

    feat(firebase-auth): add AuthContext to Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,30 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다");
+  } catch (error: any) {
+    console.log(error);
+    toast.error(error?.code);
+  }
+};
+
 export default function Profile() {
-  const auth = getAuth(app);
-  const onSignOut = async () => {
-    try {
-      await signOut(auth);
-      toast.success("로그아웃 되었습니다");
-    } catch (error: any) {
-      console.log(error);
-      toast.error(error?.code);
-    }
-  };
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image"></div>
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">
-            {auth?.currentUser?.displayName || "unknown"}
-          </div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || "unknown"}</div>
         </div>
       </div>
       <div role="presentation" className="profile__logout" onClick={onSignOut}>
