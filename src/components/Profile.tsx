1324dd2e0504ee1c25845c0e65d666a5b5e5b653
qkdflrgs <qkdflrgs@gmail.com>
2024-04-04 16:21:07 +0900

    feat(firebase-auth): add signOut to Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,33 @@
-import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 export default function Profile() {
+  const auth = getAuth(app);
+  const onSignOut = async () => {
+    try {
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다");
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.code);
+    }
+  };
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image"></div>
         <div>
-          <div className="profile__email">abc@gmail.com</div>
-          <div className="profile__name">abc</div>
+          <div className="profile__email">{auth?.currentUser?.email}</div>
+          <div className="profile__name">
+            {auth?.currentUser?.displayName || "unknown"}
+          </div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">
+      <div role="presentation" className="profile__logout" onClick={onSignOut}>
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 }
