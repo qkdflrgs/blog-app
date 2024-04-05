a363335a0b75c6f14e459c860852959f1f7f421a
qkdflrgs <qkdflrgs@gmail.com>
2024-04-05 17:40:56 +0900

    feat(comment): add submit and delete fuction to Comments component

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CommentsInterface, PostProps } from "./PostList";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
+import { toast } from "react-toastify";
 
-export default function Comments() {
+interface CommentsProps {
+  post: PostProps;
+  getPost: (id: string) => Promise<void>;
+}
+
+export default function Comments({ post, getPost }: CommentsProps) {
   const [comment, setComment] = useState<string>("");
+  const { user } = useContext(AuthContext);
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const {
@@ -13,9 +24,61 @@ export default function Comments() {
     }
   };
 
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if (post && post?.id) {
+        const postRef = doc(db, "posts", post.id);
+
+        if (user?.uid) {
+          const commentObj = {
+            content: comment,
+            uid: user.uid,
+            email: user.email,
+            createAt: new Date().toLocaleDateString("ko", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }),
+          };
+
+          await updateDoc(postRef, {
+            comments: arrayUnion(commentObj),
+            updateAt: new Date().toLocaleDateString("ko", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }),
+          });
+          await getPost(post.id);
+        }
+      }
+      toast.success("댓글을 생성했습니다");
+      setComment("");
+    } catch (e: any) {
+      console.log(e);
+      toast.error(e?.code);
+    }
+  };
+
+  const handleDeleteComment = async (data: CommentsInterface) => {
+    const confirm = window.confirm("해당 댓글을 삭제하시겠습니까?");
+
+    if (confirm && post.id) {
+      const postRef = doc(db, "posts", post.id);
+      await updateDoc(postRef, {
+        comments: arrayRemove(data),
+      });
+
+      toast.success("댓글을 삭제했습니다");
+      await getPost(post.id);
+    }
+  };
+
   return (
     <div className="comments">
-      <form className="comments__form">
+      <form className="comments__form" onSubmit={onSubmit}>
         <div className="form__block">
           <label htmlFor="comment">댓글 입력</label>
           <textarea
@@ -31,16 +94,24 @@ export default function Comments() {
         </div>
       </form>
       <div className="comments__list">
-        <div className="comment__box">
-          <div className="comment__profile-box">
-            <div className="comment__email">이메일</div>
-            <div className="comment__date">날짜</div>
-            <div className="comment__delete" role="presentation">
-              삭제
+        {post?.comments?.map((comment) => (
+          <div key={comment.createAt} className="comment__box">
+            <div className="comment__profile-box">
+              <div className="comment__email">{comment.email}</div>
+              <div className="comment__date">{comment.createAt}</div>
+              {comment.uid === user?.uid && (
+                <div
+                  className="comment__delete"
+                  role="presentation"
+                  onClick={() => handleDeleteComment(comment)}
+                >
+                  삭제
+                </div>
+              )}
             </div>
+            <div className="comment__text">{comment.content}</div>
           </div>
-          <div className="comment__text">할 말이 없습니다</div>
-        </div>
+        ))}
       </div>
     </div>
   );
